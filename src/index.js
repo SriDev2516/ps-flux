e711abc2d352f73aaa240890564fc1584bb0ad45
Srinivas Annavarpu <srini@Srinivass-MacBook-Pro.local>
2023-02-04 14:44:04 -0800

    Added HomePage and AboutPage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import AboutPage from "./components/AboutPage";
+import HomePage from "./components/HomePage";
 
+render(<AboutPage />, document.getElementById("root"));
