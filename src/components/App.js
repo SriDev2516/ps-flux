c09532ad10fcf66dbd83c1bd154e6322e0159ff7
Srinivas Annavarpu <srini@Srinivass-MacBook-Pro.local>
2023-02-07 21:18:41 -0800

    Added React for the navigation to different components

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,15 @@ import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
 import HomePage from "./HomePage";
-function getPage() {
-  const path = window.location.pathname;
-  if (path === "/about") return <AboutPage />;
-  if (path === "/courses") return <CoursesPage />;
-  return <HomePage />;
-}
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route path="/" exact component={HomePage}></Route>
+      <Route path="/courses" component={CoursesPage}></Route>
+      <Route path="/about" component={AboutPage}></Route>
     </div>
   );
 }
